8e38bcc83ac056247b2f8ee2a46e17f52847cd0b
Erik Behrends <behrends@users.noreply.github.com>
2024-06-06 08:33:49 +0200

    Update metadata

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,9 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Next.js with SQLite',
-  description: 'Demo showing Next.js with SQLite',
+  title: 'Next.js (SQLite, Lucia)',
+  description:
+    'Demo showing Next.js with SQLite using Lucia for authentication.',
 };
 
 export default function RootLayout({ children }) {
